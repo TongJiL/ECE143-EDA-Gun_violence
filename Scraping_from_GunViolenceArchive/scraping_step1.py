b6fb8b0bef33568da6f126795af86e7daa3f21b4
saideep <saideep@truenorthco.in>
2019-03-07 13:10:34 -0800

    docstrings modified and main method added

--- Scraping_from_GunViolenceArchive/scraping_step1.py
+++ Scraping_from_GunViolenceArchive/scraping_step1.py
@@ -34,38 +34,43 @@ def text_list_from_html(body):
     visible_texts = filter(tag_visible, texts)  
     return [x.strip().rstrip('<').lstrip('>') for x in visible_texts if (x != '\n') and (x != '>\n')]
 
-#######
-# Read the avaialble URLs from Kaggle
-#######
-df = pd.read_csv('gun-violence-data_01-2013_03-2018.csv')
-urls = df['incident_url'].tolist()
 
 
-######
-# Scraping from the URLs
-######
-incident_id = 0
-data_list=[]
-for url in urls[0:200]:
-    incident_id+=1
-    data_dict={}
-    data_dict['incident_id']=incident_id
-    req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"}) 
-    con = urllib2.urlopen( req )
-    html=str(con.read())
-    soup_in=html.split('block-system-main')[1:]
-    num_of_subheaders = len(soup_in[0].split("h2"))
-    elements = soup_in[0].split("h2")
-    data_dict['n_killed']=''.join(text_list_from_html(elements[4])).count("Killed")
-    data_dict['n_injured'] = ''.join(text_list_from_html(elements[4])).count("Injured")
-    loc =  [x for x in text_list_from_html(elements[2]) if any(s in x for s in list_of_states)][0].split(',')
-    data_dict['city/county']=''.join(loc[0:-1])
-    data_dict['state']=loc[-1]
-    data_dict['date'] = text_list_from_html(elements[2])[0]
-    [lati,longi] =  text_list_from_html(elements[2])[-2][13:].split(',')
-    data_dict['latitude'] = lati
-    data_dict['longitude'] = longi
-    data_list.append(data_dict)
+if __name__ == '__main__':
 
+    '''
+    # Read the avaialble URLs from Kaggle
+    '''
+    df = pd.read_csv('gun-violence-data_01-2013_03-2018.csv')
+    urls = df['incident_url'].tolist()
     
-    
+
+    '''
+    # Scraping from the URLs
+    '''
+
+    incident_id = 0
+    data_list=[]
+    for url in urls[0:200]:
+        incident_id+=1
+        data_dict={}
+        data_dict['incident_id']=incident_id
+        req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"}) 
+        con = urllib2.urlopen( req )
+        html=str(con.read())
+        soup_in=html.split('block-system-main')[1:]
+        num_of_subheaders = len(soup_in[0].split("h2"))
+        elements = soup_in[0].split("h2")
+        data_dict['n_killed']=''.join(text_list_from_html(elements[4])).count("Killed")
+        data_dict['n_injured'] = ''.join(text_list_from_html(elements[4])).count("Injured")
+        loc =  [x for x in text_list_from_html(elements[2]) if any(s in x for s in list_of_states)][0].split(',')
+        data_dict['city/county']=''.join(loc[0:-1])
+        data_dict['state']=loc[-1]
+        data_dict['date'] = text_list_from_html(elements[2])[0]
+        [lati,longi] =  text_list_from_html(elements[2])[-2][13:].split(',')
+        data_dict['latitude'] = lati
+        data_dict['longitude'] = longi
+        data_list.append(data_dict)
+
+        
+        
