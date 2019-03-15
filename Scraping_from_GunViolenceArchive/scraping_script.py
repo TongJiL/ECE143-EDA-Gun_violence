b68ff6a31347ec3f8f6139e23c3367c4f72ecada
saideep <saideepreddy91@gmail.com>
2019-03-14 21:40:10 -0700

    data Mental Health and minor changes in scraping script

--- Scraping_from_GunViolenceArchive/scraping_script.py
+++ Scraping_from_GunViolenceArchive/scraping_script.py
@@ -6,6 +6,22 @@ import pandas as pd
 from collections import defaultdict
 from bs4.element import Comment
 
+
+def text_list_from_html(body):
+	'''
+	Function to filter out the non-text elements
+	First it parses using the beautifulsoup module
+	Input:
+		body of the raw html
+	Output
+		list of visible texts
+	'''
+    assert isinstance(body,str)
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return [x.strip().rstrip('<').lstrip('>') for x in visible_texts if (x != '\n') and (x != '>\n')]
+
 def tag_visible(element):
 	'''
 	Remove unnecessary tags from html
@@ -20,22 +36,6 @@ def tag_visible(element):
         return False
     return True
 
-def text_list_from_html(body):
-	'''
-	Function to filter out the non-text elements
-	First it parses using the beautifulsoup module
-	Input:
-		body of the raw html
-	Output
-		list of visible texts
-	'''
-    soup = BeautifulSoup(body, 'html.parser')
-    texts = soup.findAll(text=True)
-    visible_texts = filter(tag_visible, texts)  
-    return [x.strip().rstrip('<').lstrip('>') for x in visible_texts if (x != '\n') and (x != '>\n')]
-
-
-
 if __name__ == '__main__':
 
     '''
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     '''
     df = pd.read_csv('gun-violence-data_01-2013_03-2018.csv')
     urls = df['incident_url'].tolist()
-    
+    list_of_states = df['state'].unique().tolist()
 
     '''
     # Scraping from the URLs
@@ -51,7 +51,7 @@ if __name__ == '__main__':
 
     incident_id = 0
     data_list=[]
-    for url in urls[0:200]:
+    for url in urls:
         incident_id+=1
         data_dict={}
         data_dict['incident_id']=incident_id
@@ -61,13 +61,13 @@ if __name__ == '__main__':
         soup_in=html.split('block-system-main')[1:]
         num_of_subheaders = len(soup_in[0].split("h2"))
         elements = soup_in[0].split("h2")
-        data_dict['n_killed']=''.join(text_list_from_html(elements[4])).count("Killed")
-        data_dict['n_injured'] = ''.join(text_list_from_html(elements[4])).count("Injured")
+        data_dict['n_killed']=''.join(text_list_from_html(elements[4])).count("Killed") # Num of killed
+        data_dict['n_injured'] = ''.join(text_list_from_html(elements[4])).count("Injured") # Num of injured
         loc =  [x for x in text_list_from_html(elements[2]) if any(s in x for s in list_of_states)][0].split(',')
-        data_dict['city/county']=''.join(loc[0:-1])
-        data_dict['state']=loc[-1]
-        data_dict['date'] = text_list_from_html(elements[2])[0]
-        [lati,longi] =  text_list_from_html(elements[2])[-2][13:].split(',')
+        data_dict['city/county']=''.join(loc[0:-1]) # City/County
+        data_dict['state']=loc[-1] # State
+        data_dict['date'] = text_list_from_html(elements[2])[0] # Date
+        [lati,longi] =  text_list_from_html(elements[2])[-2][13:].split(',') # Latitude and Longitude
         data_dict['latitude'] = lati
         data_dict['longitude'] = longi
         data_list.append(data_dict)
